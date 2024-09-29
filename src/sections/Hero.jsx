68fb3550c3c72de37231c0e284a2b22f641434f7
developerashikur1 <masik4506@gmail.com>
2024-09-30 01:47:32 +0600

    3d model hero leva added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,11 +15,35 @@ export default function Hero() {
             max: 10,
         },
         positionY: {
-            value: 0,
+            value: 2.5,
+            min: -10,
+            max: 10,
         },
         positionZ: {
-            
-        }
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        scale: {
+            value: 1,
+            min: 0.1,
+            max: 10,
+        },
     })
     
     return (
@@ -29,16 +53,15 @@ export default function Hero() {
                 <p className="hero_tag text-gray_gradient">Building Products and brands</p>
 
                 <div className="w-full h-full absolute inset-0">
+                    <Leva />
                     <Canvas className="w-full h-full0">
                         <PerspectiveCamera makeDefault position={[0, 0, 30]} />
                         <Suspense fallback={<CanvasLoader />}>
                             <MyHackerRoom
-                                position={[0, 0, 0]}
-                                rotation={[0, -Math.PI / 2, 0]}
-                                scale={[controls.positionX, controls.positionY, controls.positionZ]}
+                                position={[controls.positionX, controls.positionY, controls.positionZ]}
+                                rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
+                                scale={[controls.scale, controls.scale, controls.scale]}
                             />
-                            <Leva />
-                            
                             <ambientLight intensity={1} />
                             <directionalLight position={[10, 10, 10]} intensity={0.5} />
                         </Suspense>
